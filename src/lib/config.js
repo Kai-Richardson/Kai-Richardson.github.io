e8e03e57721c28b04bb80f12aa14f2859a4721a6
Kai Richardson <kairichardson@gmail.com>
2023-06-18 16:17:40 -0700

    `npm run format`

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,29 +1,29 @@
 /**
- * All of these values are used throughout the site – for example, 
+ * All of these values are used throughout the site – for example,
  * in the <meta> tags, in the footer, and in the RSS feed.
- * 
- * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
- **/ 
+ **/
 
-export const siteTitle = 'My Awesome Blog'
-export const siteDescription = 'Built with the SvelteKit Static Blog Starter'
-export const siteURL = 'example.com'
-export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
-export const siteAuthor = '- find and change this text in src/lib/config.js'
+export const siteTitle = 'Kai Richardson&apos;s Website';
+export const siteDescription = 'FOOBAR';
+export const siteURL = 'kairichardson.com';
+export const siteLink = 'https://github.com/Kai-Richardson/Kai-Richardson.github.io';
+export const siteAuthor = '- Kai Richardson';
 
 // Controls how many posts are shown per page on the main blog index pages
-export const postsPerPage = 10
+export const postsPerPage = 10;
 
 // Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
 export const navItems = [
 	{
-		title: 'Blog',
-		route: '/blog'
-	}, {
 		title: 'About',
 		route: '/about'
-	}, {
-		title: 'Contact',
-		route: '/contact' 
 	},
-]
+	{
+		title: 'Blog',
+		route: '/blog'
+	},
+	{
+		title: 'Contact',
+		route: '/contact'
+	}
+];
