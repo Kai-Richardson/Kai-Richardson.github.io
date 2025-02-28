88e711c80644bef4f32c5a65834dd5ce90eacd8a
Kai Richardson <kairichardson@gmail.com>
2025-02-28 22:48:21 +0000

    questions page v0.9

---
+++ src/routes/questions/+page.svelte
@@ -0,0 +1,66 @@
+<svelte:head>
+	<title>Asking Questions</title>
+
+	<meta name="twitter:image:src" content="">
+	<meta name="twitter:site" content="Kai's Zone">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:title" content="Getting help with development">
+	<meta name="twitter:description" content="">
+</svelte:head>
+
+
+<h1>Getting help with development</h1>
+<div class="centered-container">
+    <div class="questions">
+        <h2>#1 Don't ask to ask</h2>
+        <p>
+            Don't start speaking with “May I ask a question?” or “Can anyone help me?”<br>
+            Although that may be polite in normal human interaction, it does not work that way asynchronously.
+            Just ask your question.
+        </p>
+        <h2>#2 Be precise</h2>
+        <p>Include as much detail as possible.
+            This includes where  you experienced the issue and the date that it occurred. Your timezone can
+            also be helpful, as developers can be internationally located.</p>
+        <h2>#3 Explain what you're trying to achieve</h2>
+        <p>
+            This way we can tell you if what you're currently doing makes sense, or if you'd have to do something
+            else anyways.<br>
+            This is much better rather than spending ages helping you only for us both to find out it won't help you
+            at all.
+        </p>
+        <h2>#4 Include the code</h2>
+        <p>If you're asking about code, take a screenshot, upload a file, or use a pastebin.<br>
+            However you do it, include the snippet - we're only going to have to ask you anyway.</p>
+        <h3>#3a Show entire files</h3>
+        <p>
+            Please don't try to embed many lines of code into chat, it won't help anyone.<br>
+            It's fine if it's only a handful.
+        </p>
+        <h3>#3b Include full errors</h3>
+        <p>
+            If you're getting an error, put it in a file.
+            The full error will give more info to help you debug what went wrong.
+        </p>
+        <h2>#5 Don't ping specific people</h2>
+        <p>If it's very urgent - sure, otherwise just put your question out with no pings.<br>
+            People are more likely to respond if you don't ask a specific person as they won't feel like they're
+            stepping on someone elses toes.<br>
+            Furthermore, there are likely specialists with regard to the topic that you don't know about who will
+            reveal themselves by responding.</p>
+        <h2>#6 Try the obvious Google search first</h2>
+        <p>
+            People are less likely to help you if you are asking questions that are answerable by a 1 minute Google search.
+        </p>
+        <h2>#7 Wait for the answers</h2>
+        <p>
+            An answer might arrive now, or it might arrive in the next day. Be patient and wait an hour before
+            asking again. People can answer when you're offline and they come online.
+        </p>
+        <h2>#8 Answer questions in a timely fashion</h2>
+        <p>
+            If someone asks you a question, try to answer it as soon as you can, otherwise let them know you if you
+            cant or you don't know how, this will help us help you.
+        </p>
+    </div>
+</div>
